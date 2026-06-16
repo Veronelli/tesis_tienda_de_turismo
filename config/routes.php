f3f403889ed5c39394856bbf7aff8d0e6aa94669
Quimeydl <quimeydl143@gmail.com>
2026-06-16 18:55:43 -0300

    rutas de hoteles

--- config/routes.php
+++ config/routes.php
@@ -6,11 +6,12 @@ use Symfony\Component\Routing\RouteCollection;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\AuthController;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\DestinoController;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\HealthController;
+use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\HotelController;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\LoginController;
 
 $routes = new RouteCollection();
 
-foreach ([HealthController::class, LoginController::class, AuthController::class, DestinoController::class] as $controller) {
+foreach ([HealthController::class, LoginController::class, AuthController::class, DestinoController::class, HotelController::class] as $controller) {
     $routes->addCollection($controller::rutas());
 }
 
