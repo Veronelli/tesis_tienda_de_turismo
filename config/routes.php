8d2998493f045ac32d9b0ee3722f62b86a3467e5
Quimeydl <quimeydl143@gmail.com>
2026-06-18 18:59:06 -0300

    Conexiones de clientes

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Symfony\Component\Routing\RouteCollection;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\AuthController;
+use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\ClienteController;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\DestinoController;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\HealthController;
 use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\HotelController;
@@ -11,7 +12,7 @@ use TiendaTurismo\GestionDatos\Interfaces\Http\Controllers\LoginController;
 
 $routes = new RouteCollection();
 
-foreach ([HealthController::class, LoginController::class, AuthController::class, DestinoController::class, HotelController::class] as $controller) {
+foreach ([HealthController::class, LoginController::class, AuthController::class, DestinoController::class, HotelController::class, ClienteController::class] as $controller) {
     $routes->addCollection($controller::rutas());
 }
 
